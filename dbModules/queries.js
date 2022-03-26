2b546495da518ee4db795c710e39cb25cf3dd268
baloghbalint888 <baloghbalint888@gmail.com>
2022-03-26 21:27:08 +0100

    "added routing and middlewares"

--- dbModules/queries.js
+++ dbModules/queries.js
@@ -1,6 +1,6 @@
 const mysql = require('mysql');
 const config = require('./config.json')
-
+const bcrypt = require("bcrypt");
 const connection = mysql.createConnection({
     host: config.host,
     port: config.port,
@@ -8,104 +8,159 @@ const connection = mysql.createConnection({
     user: config.user,
     password: config.password
 });
-function vat(){
+
+//bruttó árgenerátor szenvedés
+/* function vat(){
     myQuery = `SELECT vat_percentage FROM vat`;
     connection.query(myQuery,(err,result,fields)=>{
-        if(err)
-            callback(err,null);
-        else{
-            callback(null,JSON.parse(JSON.stringify(result)));
+        const data = JSON.parse(JSON.stringify(result))
+        let percentage = data[0].vat_percentage;
+        return percentage;
+    })
+
+}
+ */
+module.exports.adminList = function (callback) {
+    myQuery = `SELECT name, login FROM admin`;
+    connection.query(myQuery, (err, result, fields) => {
+        if (err)
+            callback(err, null);
+        else {
+            callback(null, JSON.parse(JSON.stringify(result)));
         }
     })
 }
 
-
-
-module.exports.userList = function(callback){
+module.exports.userList = function (callback) {
     myQuery = `SELECT login, name, email FROM USERS`;
-    connection.query(myQuery,(err,result,fields)=>{
-        if(err)
-            callback(err,null);
-        else{
-            callback(null,JSON.parse(JSON.stringify(result)));
+    connection.query(myQuery, (err, result, fields) => {
+        if (err)
+            callback(err, null);
+        else {
+            callback(null, JSON.parse(JSON.stringify(result)));
         }
     })
 }
 
-module.exports.user = function(id,callback){
+module.exports.user = function (id, callback) {
     myQuery = `SELECT login, phone, birth FROM USERS WHERE userID = ${id}`;
-    connection.query(myQuery,(err,result,fields)=>{
-        if(err)
-            callback(err,null);
-        else{
-            callback(null,JSON.parse(JSON.stringify(result)));
+    connection.query(myQuery, (err, result, fields) => {
+        if (err)
+            callback(err, null);
+        else {
+            callback(null, JSON.parse(JSON.stringify(result)));
         }
     })
 }
 
-module.exports.productList = function(callback){
-    myQuery = `SELECT name,picture,net_value FROM distribution`;
-    connection.query(myQuery,(err,result,fields)=>{
-        if(err)
-            callback(err,null);
-        else{
-            callback(null,JSON.parse(JSON.stringify(result)));
+module.exports.addUser = function (data, callback) {
+    const salt = bcrypt.genSaltSync(10);
+    const hashedPW = bcrypt.hashSync(data.password, salt)
+    myQuery = `INSERT INTO users (login,password,name,phone,birth,email,billing_address,shipping_address) VALUES ('${data.login}','${hashedPW}','${data.name}','${data.phone}','${data.birth}','${data.email}','${data.billing_address}','${data.shipping_address}')`;
+    connection.query(myQuery, (err, result, fields) => {
+        if (err)
+            callback(err, null);
+        else {
+            callback(null, JSON.parse(JSON.stringify(result)));
         }
     })
 }
 
-module.exports.productList = function(id,callback){
+module.exports.delUser = function (id, callback) {
+    myQuery = `DELETE FROM users WHERE userID = ${id}`;
+    connection.query(myQuery, (err, result, fields) => {
+        if (err)
+            callback(err, null);
+        else {
+            callback(null, JSON.parse(JSON.stringify(result)));
+        }
+    })
+}
+
+module.exports.delUserName = function (login, callback) {
+    myQuery = `DELETE FROM users WHERE login = '${login}'`;
+    connection.query(myQuery, (err, result, fields) => {
+        if (err)
+            callback(err, null);
+        else {
+            callback(null, JSON.parse(JSON.stringify(result)));
+        }
+    })
+}
+module.exports.productList = function (callback) {
+    myQuery = `SELECT name,description,picture,net_value FROM distribution`;
+    connection.query(myQuery, (err, result, fields) => {
+        if (err)
+            callback(err, null);
+        else {
+            callback(null, JSON.parse(JSON.stringify(result)));
+        }
+    })
+}
+
+module.exports.product = function (id, callback) {
     myQuery = `SELECT name,picture,net_value FROM distribution WHERE productID = ${id}`;
-    connection.query(myQuery,(err,result,fields)=>{
-        if(err)
-            callback(err,null);
-        else{
-            callback(null,JSON.parse(JSON.stringify(result)));
+    connection.query(myQuery, (err, result, fields) => {
+        if (err)
+            callback(err, null);
+        else {
+            callback(null, JSON.parse(JSON.stringify(result)));
         }
     })
 }
 
-module.exports.serviceList = function(callback){
+module.exports.addProduct = function (data, callback) {
+    myQuery = `INSERT INTO distribution (catID,name,amount, picture,description,vat_id,net_value) VALUES (${data.catID},'${data.name}','${data.amount}','${data.picture}','${data.description}',${data.vat_id},${data.net_value})`;
+    connection.query(myQuery, (err, result, fields) => {
+        if (err)
+            callback(err, null);
+        else {
+            callback(null, JSON.parse(JSON.stringify(result)));
+        }
+    })
+}
+
+module.exports.serviceList = function (callback) {
     myQuery = `SELECT * FROM services`;
-    connection.query(myQuery,(err,result,fields)=>{
-        if(err)
-            callback(err,null);
-        else{
-            callback(null,JSON.parse(JSON.stringify(result)));
+    connection.query(myQuery, (err, result, fields) => {
+        if (err)
+            callback(err, null);
+        else {
+            callback(null, JSON.parse(JSON.stringify(result)));
         }
     })
 }
 
-module.exports.service = function(id,callback){
+module.exports.service = function (id, callback) {
     myQuery = `SELECT * FROM services WHERE serviceID = ${id}`;
-    connection.query(myQuery,(err,result,fields)=>{
-        if(err)
-            callback(err,null);
-        else{
-            callback(null,JSON.parse(JSON.stringify(result)));
+    connection.query(myQuery, (err, result, fields) => {
+        if (err)
+            callback(err, null);
+        else {
+            callback(null, JSON.parse(JSON.stringify(result)));
         }
     })
 }
 
 
-module.exports.categoryList = function(callback){
+module.exports.categoryList = function (callback) {
     myQuery = `SELECT * FROM categories`;
-    connection.query(myQuery,(err,result,fields)=>{
-        if(err)
-            callback(err,null);
-        else{
-            callback(null,JSON.parse(JSON.stringify(result)));
+    connection.query(myQuery, (err, result, fields) => {
+        if (err)
+            callback(err, null);
+        else {
+            callback(null, JSON.parse(JSON.stringify(result)));
         }
     })
 }
 
-module.exports.category = function(id,callback){
+module.exports.category = function (id, callback) {
     myQuery = `SELECT * FROM categories WHERE id = ${id}`;
-    connection.query(myQuery,(err,result,fields)=>{
-        if(err)
-            callback(err,null);
-        else{
-            callback(null,JSON.parse(JSON.stringify(result)));
+    connection.query(myQuery, (err, result, fields) => {
+        if (err)
+            callback(err, null);
+        else {
+            callback(null, JSON.parse(JSON.stringify(result)));
         }
     })
 }
--- index.js
+++ index.js
@@ -1,11 +1,23 @@
+
+
 /*
 GET /users -> list of all users 
 GET /users/:id -> get one user by specifying a parameter (id)
 GET /products -> list of all products
 GET /products/:id -> get one product by specifying a parameter (id)
-GET
-GET
+GET /services/ -> list of all services
+GET /services/:id -> get one service by specifying a paramater (id)
+GET /categories/ -> list of all services
+GET /categories/:id -> get one service by specifying a paramater (id)
+GET /admin/ -> list of all admins
+POST /addUser -> adds a user from a recieved JSON object
+POST /addProduct -> adds a product from a recieved JSON object
+
+DELETE /delUser -> deletes 1 user by specifying login name
+DELETE /delUser/:id -> deletes 1 user by specifying id
+
 */
+
 
 const dbData = require('./dbModules/queries');
 const express = require('express');
@@ -20,21 +32,56 @@ const app = express();
 app.use(express.json())
 app.use(cors({ origin: "http://localhost:3000" }));
 
-app.get('/users', (req, res) => {
-     dbData.userList((err, data) => {
+app.get('/admin', (req, res) => {
+    dbData.adminList((err, data) => {
         if (err) throw err;
-        
+        console.log("Új kérés a /admin felé..")
+        res.json(data);
+    })
+})
+
+app.get('/admin/:id', (req, res) => {
+    dbData.adminList((err, data) => {
+        if (err) throw err;
+
+        res.json(data);
+    })
+})
+
+app.get('/users', (req, res) => {
+    dbData.userList((err, data) => {
+        if (err) throw err;
+        console.log('Új kérés a /users felé')
         res.json(data);
     })
 })
 
 app.get('/users/:id', (req, res) => {
-    dbData.user(req.params.id,(err, data) => {
-       if (err) throw err;
-       res.json(data);
-   })
+    dbData.user(req.params.id, (err, data) => {
+        if (err) throw err;
+        res.json(data);
+    })
+})
+app.post('/addUser', (req, res) => {
+    dbData.addUser(req.body, (err, data) => {
+        if (err) throw err;
+        res.json({ "status": "ok" });
+    })
 })
 
+app.delete('/delUser', (req, res) => {
+    dbData.delUserName(req.body.login, (err, data) => {
+        if (err) throw err;
+        res.json({ "deleted": "ok" })
+    })
+})
+
+app.delete('/delUser/:id', (req, res) => {
+    dbData.delUser(req.params.id, (err, data) => {
+        if (err) throw err;
+        res.json({ "deleted": "ok" })
+    })
+})
 app.get('/products', (req, res) => {
     dbData.productList((err, data) => {
         if (err) throw err;
@@ -43,9 +90,16 @@ app.get('/products', (req, res) => {
 })
 
 app.get('/products/:id', (req, res) => {
-    dbData.product(req.params.id,(err, data) => {
+    dbData.product(req.params.id, (err, data) => {
         if (err) throw err;
         res.json(data);
+    })
+})
+
+app.post("/addProduct", (req, res) => {
+    dbData.addProduct(req.body, (err, data) => {
+        if (err) throw err;
+        res.json({ "added": "ok" });
     })
 })
 
@@ -55,9 +109,8 @@ app.get('/services', (req, res) => {
         res.json(data);
     })
 })
-
 app.get('/services/:id', (req, res) => {
-    dbData.service(req.params.id,(err, data) => {
+    dbData.service(req.params.id, (err, data) => {
         if (err) throw err;
         res.json(data);
     })
@@ -72,10 +125,12 @@ app.get('/categories', (req, res) => {
 })
 
 app.get('/categories/:id', (req, res) => {
-    dbData.category(req.params.id,(err, data) => {
+    dbData.category(req.params.id, (err, data) => {
         if (err) throw err;
         res.json(data);
     })
 })
 
-app.listen(port);
+app.listen(port, () => {
+    console.log(`Running on port: ${port}`)
+});
